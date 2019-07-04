a2741d476a2b12230ed9b295d6197d18c27b7284
Nathan Alan <nathan@oxil.co.uk>
2019-07-04 12:19:48 +0100

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kiniauth-angular'`, () => {
+  it(`should have as title 'admin'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('kiniauth-angular');
+    expect(app.title).toEqual('admin');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to kiniauth-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to admin!');
   });
 });
